d331287edba994eeb1e28daf4243cdb47691a3ba
jaelygarcia <107519725+jaelygarcia@users.noreply.github.com>
2022-06-15 23:28:35 -0400

    Update calculo_notas.rb

--- desafios_areglos_archivos/calculo_notas.rb
+++ desafios_areglos_archivos/calculo_notas.rb
@@ -8,6 +8,6 @@ def highest_grades(array)
   puts "La calificación más alta de #{array[0]} es #{array[1,4].max}"
 end
 
-data = read_file("d07_grades.db")
+data = read_file("notas.db")
 highest_grades(data[0]) # = 9
 highest_grades(data[1]) # = 8
