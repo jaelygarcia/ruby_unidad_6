c799bf738d2d6bad666d4d8dc70f6bd54e8905d2
jaelygarcia <107519725+jaelygarcia@users.noreply.github.com>
2022-06-15 23:22:16 -0400

    Se agrega desafío

---
+++ desafios_areglos_archivos/proyecciones.rb
@@ -0,0 +1,25 @@
+def read_file(filename)
+  data = open(filename).read.split(",").map {|i| i.to_f}
+  lines = data.length
+  newData = []
+  lines.times do |i|
+    newData.push data[i]
+  end
+  newData
+end
+
+def projections(array, increase, startIndex, endIndex)
+  results = array[startIndex, endIndex].map {|i| sprintf('%.2f',(i * increase))}
+end
+
+def write_file(array)
+  File.write("d06_results.db", array.join("\n"))
+end
+
+arraySales = read_file("d06_baseSales.db")
+firstSemester = projections(arraySales, 1.1, 0, 5)
+secondSemester = projections(arraySales, 1.2, 6, 11)
+
+nextYearSales = firstSemester + secondSemester
+
+write_file(nextYearSales)
