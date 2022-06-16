8e51af5a18792873a17aceb8bd2635f2073344cb
jaelygarcia <107519725+jaelygarcia@users.noreply.github.com>
2022-06-15 23:26:41 -0400

    Se agrega desafío

---
+++ desafios_areglos_archivos/calculo_notas.rb
@@ -0,0 +1,13 @@
+require 'csv'
+
+def read_file(filename)
+  data = CSV.open(filename, converters: :numeric).readlines
+end
+
+def highest_grades(array)
+  puts "La calificación más alta de #{array[0]} es #{array[1,4].max}"
+end
+
+data = read_file("d07_grades.db")
+highest_grades(data[0]) # = 9
+highest_grades(data[1]) # = 8
